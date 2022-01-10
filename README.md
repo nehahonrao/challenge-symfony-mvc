9b19fd593cda95305afea6eab7894b25d9f7317c
unknown <nehalonkar27@gmail.com>
2022-01-10 12:33:50 +0100

    installled symfony

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # challenge-symfony-mvc
+## Date:10-01-2022
+
+## Install Symfony
+- First I installed composer.
+- for installing symfony-cli I have to installed first GoFish,which is a cross platform systems package manager.
+- https://gofi.sh/ followed this link to install GoFish
+- then installed symfony-cli with help of following commands:
+ - 1. gofish rig add https://github.com/symfony-cli/fish-food
+ - 2. gofish install github.com/symfony-cli/fish-food/symfony-cli
+ - created new symfony application: symfony new project-name.
+ - by using symfony serve command i can see symfony page on brower.
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '27a458352bb78579d5dcceb5743c6aa55b0e845b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '27a458352bb78579d5dcceb5743c6aa55b0e845b',
+            'dev_requirement' => false,
+        ),
+        'symfony/apache-pack' => array(
+            'pretty_version' => 'v1.0.1',
+            'version' => '1.0.1.0',
+            'type' => 'symfony-pack',
+            'install_path' => __DIR__ . '/../symfony/apache-pack',
+            'aliases' => array(),
+            'reference' => '3aa5818d73ad2551281fc58a75afd9ca82622e6c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
